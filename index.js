ac239dd78103a6d87ebaee04261aed06e1ac2c1c
Victor Conner <charlesvictorconner@gmail.com>
2020-12-19 11:19:13 +0100

    Refactor constants to be called templates

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const transform = require('./transform')
 const render = require('./render')
 
 // == TEMPLATES ================================================================
-const generalTemplate = yaml.parse(fs.readFileSync('./constants/general.yml', 'utf-8'))
-const bashTemplate = yaml.parse(fs.readFileSync('./constants/bash.yml', 'utf-8'))
+const generalTemplate = yaml.parse(fs.readFileSync('./templates/general.yml', 'utf-8'))
+const bashTemplate = yaml.parse(fs.readFileSync('./templates/bash.yml', 'utf-8'))
 
 // == SECTIONS =================================================================
 const sections = { general: transform(generalTemplate), bash: transform(bashTemplate) }
