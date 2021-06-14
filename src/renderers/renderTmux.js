cc379d305afbce5cf809940500ee934cd5e7caa4
creativecreature <hello@creativecreature.com>
2021-06-14 14:39:30 +0200

    Made improvements to the TMUX renderer

--- src/renderers/renderTmux.js
+++ src/renderers/renderTmux.js
@@ -1,29 +1,52 @@
 const fs = require('fs')
 
-const createArrow = (foreground, background, direction) => `#[fg=#${foreground},bg=${background},nobold,noitalics,nounderscore]${direction === 'left' ? '' : ''}`
+const createArrow = (foreground, background, direction) =>
+  `#[fg=#${foreground},bg=${background},nobold,noitalics,nounderscore]${direction === 'left' ? '' : ''}`
 
-const renderStatusLeft = tmux => `\
+const renderStatus = ({ status }) => `\
+set -g status-bg "${status.background}"
+set -g status-style "bg=${status.background}"
 set -g status-left-length 50
-set -g status-left "#[fg=#252837,bg=#f78e6e,nobold] #S ${createArrow(tmux.statusLeftArrow.foreground, tmux.statusLeftArrow.background, 'right')}"
+set -g status-right-length 150
+`
+
+const renderStatusLeft = ({ statusLeftArrow }) => `\
+STATUS_LEFT_SUFFIX=${createArrow(statusLeftArrow.foreground, statusLeftArrow.background, 'right')}
+set -g status-left "#[fg=#252837,bg=#f78e6e,nobold] #S $STATUS_LEFT_SUFFIX"
 `
 
 // The stats will have a prefix arrow with inversed colors
-const renderStatusRight = tmux => `\
-set -g status-right-length 150
-STATS_CPU="${createArrow(tmux.statsCPU.background, tmux.statsCPU.foreground, 'left')}#[fg=${tmux.statsCPU.foreground},bg=${tmux.statsCPU.background}]#{sysstat_cpu}"
-set -g @sysstat_cpu_color_low "${tmux.statsCPULow.foreground}"
-set -g @sysstat_cpu_view_tmpl ' #[fg=${tmux.statsCPU.foreground},nobold]#{cpu.pused} '
-STATS_MEM="${createArrow(tmux.statsMem.background, tmux.statsMem.foreground, 'left')}#[fg=${tmux.statsMem.foreground},bg=${tmux.statsMem.background}]#{sysstat_mem}"
-set -g @sysstat_mem_color_low "${tmux.statsMemLow.foreground}"
-set -g @sysstat_mem_view_tmpl ' #[fg=${tmux.statsMem.foreground},nobold]#{mem.used} / #[fg=${tmux.statsMem.foreground},nobold]#{mem.total} '
-YY_MM_DD="${createArrow(tmux.date.background, tmux.date.foreground, 'left')}#[fg=${tmux.date.foreground},bg=${tmux.date.background}] %Y-%m-%d "
-H_M="${createArrow(tmux.time.background, tmux.time.foreground, 'left')}#[fg=${tmux.time.foreground},bg=${tmux.time.background}] %H:%M "
+const renderStatusRight = ({ statsCPU, statsCPULow, statsMem, statsMemLow, date, time }) => `\
+CPU_PREFIX=${createArrow(statsCPU.background, statsCPU.foreground, 'left')}
+STATS_CPU="$CPU_PREFIX#[fg=${statsCPU.foreground},bg=${statsCPU.background}]#{sysstat_cpu}"
+set -g @sysstat_cpu_color_low "${statsCPULow.foreground}"
+set -g @sysstat_cpu_view_tmpl ' #[fg=${statsCPU.foreground},nobold]#{cpu.pused} '
+
+MEM_PREFIX=${createArrow(statsMem.background, statsMem.foreground, 'left')}
+STATS_MEM="$MEM_PREFIX#[fg=${statsMem.foreground},bg=${statsMem.background}]#{sysstat_mem}"
+set -g @sysstat_mem_color_low "${statsMemLow.foreground}"
+set -g @sysstat_mem_view_tmpl ' #[fg=${statsMem.foreground},nobold]#{mem.used} / #[fg=${statsMem.foreground},nobold]#{mem.total} '
+
+DATE_PREFIX=${createArrow(date.background, date.foreground, 'left')}
+YY_MM_DD="$DATE_PREFIX#[fg=${date.foreground},bg=${date.background}] %Y-%m-%d "
+
+TIME_PREFIX=${createArrow(time.background, time.foreground, 'left')}
+H_M="$TIME_PREFIX#[fg=${time.foreground},bg=${time.background}] %H:%M "
 set -g status-right "$STATS_CPU$STATS_MEM$YY_MM_DD$H_M"
 `
 
-const renderStatusBar = tmux => `\
-set -g window-status-current-format "${createArrow(tmux.currentStatusBarPrefixArrow.foreground, tmux.currentStatusBarPrefixArrow.background, 'right')} #[fg=${tmux.currentStatusBarText.foreground},bg=${tmux.currentStatusBarText.background}]#I  #W #F ${createArrow(tmux.currentStatusBarSuffixArrow.foreground, tmux.currentStatusBarSuffixArrow.background, 'right')}"
-set -g window-status-format "${createArrow(tmux.inactiveStatusBarPrefixArrow.foreground, tmux.inactiveStatusBarPrefixArrow.background, 'right')} #[fg=${tmux.inactiveStatusBarText.foreground},bg=${tmux.inactiveStatusBarText.background} ]#I  #W #F ${createArrow(tmux.inactiveStatusBarSuffixArrow.foreground, tmux.inactiveStatusBarSuffixArrow.background, 'right')}"
+const renderCurrentStatusbar = ({ currentStatusBarPrefixArrow, currentStatusBarText, currentStatusBarSuffixArrow }) => `\
+CURRENT_STATUS_BAR_PREFIX=${createArrow(currentStatusBarPrefixArrow.foreground, currentStatusBarPrefixArrow.background, 'right')}
+CURRENT_STATUS_BAR_TEXT="#[fg=${currentStatusBarText.foreground},bg=${currentStatusBarText.background}]#I  #W #F"
+CURRENT_STATUS_BAR_SUFFIX=${createArrow(currentStatusBarSuffixArrow.foreground, currentStatusBarSuffixArrow.background, 'right')}
+set -g window-status-current-format "$CURRENT_STATUS_BAR_PREFIX $CURRENT_STATUS_BAR_TEXT $CURRENT_STATUS_BAR_SUFFIX"
+`
+
+const renderInactiveStatusbar = ({ inactiveStatusBarPrefixArrow, inactiveStatusBarText, inactiveStatusBarSuffixArrow }) => `\
+INACTIVE_STATUS_BAR_PREFIX=${createArrow(inactiveStatusBarPrefixArrow.foreground, inactiveStatusBarPrefixArrow.background, 'right')}
+INACTIVE_STATUS_BAR_TEXT="#[fg=${inactiveStatusBarText.foreground},bg=${inactiveStatusBarText.background} ]#I  #W #F"
+INACTIVE_STATUS_BAR_SUFFIX=${createArrow(inactiveStatusBarSuffixArrow.foreground, inactiveStatusBarSuffixArrow.background, 'right')}
+set -g window-status-format "$INACTIVE_STATUS_BAR_PREFIX $INACTIVE_STATUS_BAR_TEXT $INACTIVE_STATUS_BAR_SUFFIX"
 set -g window-status-separator ""
 set -g monitor-activity off
 `
@@ -38,18 +61,15 @@ setw -g pane-active-border-style "fg=${tmux.borders.foreground},bg=${tmux.border
 setw -g pane-border-style "fg=${tmux.borders.foreground},bg=${tmux.borders.background}"
 `
 
-const renderStatus = tmux => `\
-set -g status-bg "${tmux.status.background}"
-set -g status-style "bg=${tmux.status.background}"
-`
 
 const toRenderString = tmux => `\
+${renderStatus(tmux)}
 ${renderStatusLeft(tmux)}
 ${renderStatusRight(tmux)}
-${renderStatusBar(tmux)}
+${renderCurrentStatusbar(tmux)}
+${renderInactiveStatusbar(tmux)}
 ${renderWindows(tmux)}
 ${renderBorders(tmux)}
-${renderStatus(tmux)}
 `
 
 const renderTmux = (tmux) => {
